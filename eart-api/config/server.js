64d7fac7f0b29f0abc43e084584c2c1e89e143c0
MichelleCardoso0 <michelle.cardoso@novaandradina.org>
2020-12-15 18:33:05 -0400

    first commit

---
+++ eart-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ea0701aa1e39403a415a8a3b91227980'),
+    },
+  },
+});
---
+++ eart-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cc5e4ef0-6fd9-4e2d-86f1-99b94df01862'
+};
